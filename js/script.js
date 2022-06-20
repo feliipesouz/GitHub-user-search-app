be7b49f85d35bfd9db4ec4756b6b838095617364
feliipesouz <48961822+feliipesouz@users.noreply.github.com>
2022-06-19 22:39:10 -0300

    Implementação da função de DARK/LIGHT

--- js/script.js
+++ js/script.js
@@ -2,43 +2,57 @@ const inputContent = document.querySelector('input');
 const btn = document.querySelector('.searchbtn');
 let block = document.querySelector('.authorImg');
 let img = document.createElement('img');
-const user = document.querySelector('.userGit');//Dúvida
+img.id = 'avatar_url';
+img.src = './assets/png-githubcat.png';
+block.appendChild(img);
+console.log(img);
+
 const login = document.querySelector('.userNameGit');
 const joined = document.querySelector('.joinedDateGit');
-const repo = document.querySelector('.repoTotal');
-const follower = document.querySelector('.followerTotal')
-const followings = document.querySelector('.followingTotal');
-const locations = document.querySelector(".locations");
-const twit = document.querySelector(".twit");
-const websites = document.querySelector(".websites");
-const companies = document.querySelector(".companies");
-const gitBio = document.querySelector(".githubBio");
+// const repo = document.querySelector('.repoTotal');
 
 function action() {
   const url = `https://api.github.com/users/${inputContent.value}`;
-  
-  async function getUrl(){
+
+  async function getUrl() {
     const response = await fetch(url);
     const json = await response.json();
     const dateData = json.created_at.slice(0, json.created_at.length - 10);
-    
+
+    const content = document.getElementsByClassName('changeContent');
+    let arrayOfContent = Array.from(content);
+
+    console.log(arrayOfContent);
+    arrayOfContent.map((item) => {
+      item.innerHTML = !json[item.id] ? 'Não informado' : json[item.id];
+    });
+
     img.src = json.avatar_url;
-    block.appendChild(img)
-    console.log(json)
-    
-    user.innerHTML = json.name;
+    block.appendChild(img);
+    console.log(json);
+
     login.innerHTML = `@${json.login}`;
-    joined.innerHTML = `Joined ${dateData}`
-    repo.innerHTML = json.public_repos;
-    follower.innerHTML = json.followers;
-    followings.innerHTML = json.following;
-
-    locations.innerHTML = json.location == null ? 'Undefined' : json.location;
-    twit.innerHTML = json.twitter_username;
-    websites.innerHTML = json.blog;
-    companies.innerHTML = json.company;
-    gitBio.innerHTML = json.bio;
-
-  };
+    joined.innerHTML = `Joined ${dateData}`;
+  }
   getUrl();
+}
+
+const toggleTheme = () => {
+  let colorTheme = document.querySelector('html').classList.toggle('dark-mode');
+
+  if (colorTheme) {
+    let nameButton = document.querySelector('#button');
+    nameButton.textContent = 'DARK';
+    let imgButton = document.createElement('img');
+    imgButton.src = './assets/icon-moon.svg';
+    imgButton.alt = 'sun image';
+    nameButton.appendChild(imgButton);
+  } else {
+    let nameButton = document.querySelector('#button');
+    nameButton.textContent = 'LIGHT';
+    let imgButton = document.createElement('img');
+    imgButton.src = './assets/icon-sun.svg';
+    imgButton.alt = 'moon image';
+    nameButton.appendChild(imgButton);
+  }
 };
